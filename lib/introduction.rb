3548de866dc5469ffbdae50f0f76afbc2c17eeda
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-19 22:31:38 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,8 @@ def introduction（name)
   puts "Hi, my name is #{name}."
 end 
 
-introduction(yy)
 
 def introduction_with_language (name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end 
 
-introduction_with_language(zz, Spanish)
